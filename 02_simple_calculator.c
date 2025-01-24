7d88bde09ed624c8c8e84465365f7f8ed74133d9
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-24 23:32:28 +0530

    Update 02_simple_calculator.c

--- 02_simple_calculator.c
+++ 02_simple_calculator.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 
-
-int main() {
-    
-
-    int choice;
-    double num1, num2, result; 
-    // double is used to store floating point numbers with double precision.
-    
-
-    do {
+void print_menu() {
     printf("Welcome to Simple Calculator\n");
     printf("\nChoose one of the following options : \n");
     printf("1. Addition\n");
@@ -19,8 +10,31 @@ int main() {
     printf("5. Modulus\n");
     printf("6. Power\n");
     printf("7. Exit\n");
+}
+
+int main() {
+    
+
+    int choice;
+    double num1, num2, result; 
+    // double is used to store floating point numbers with double precision.
+    
+
+    while {
+    print_menu();
     
     printf("\nEnter your choice : ");
-    scanf("%d", &choice);
-    } while (choice != 7);
+    scanf("%d\n", &choice);
+
+    if (choice == 7) {
+        printf("Thank you for using the calculator\n");
+        break;
+    }
+    printf("Please enter the first numbere : ");
+    scanf("%lf", &num1);//lf is used to read double data type.
+
+    printf("Please enter the second number : ");
+    scanf("%lf", &num2);//lf is used to read double data type.
+
+    }
 }
