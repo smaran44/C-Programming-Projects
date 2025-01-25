59961458b802859cc523940004fb4b9ba9c491a1
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-25 20:11:36 +0530

    Update 03_digital_clock.c

---
+++ 03_digital_clock.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<time.h>//time.h is a header file in the C standard library for the C programming language that contains time and date function declarations to provide standardized access to time/date manipulation and formatting.
+
+int main () {
+
+    
+
+
+
+
+
+    return 0;
+}
