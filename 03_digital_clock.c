003a54cd0771ec72f270600172cb558dab557a58
Smaran Yanapu <smaranyanapu@gmail.com>
2025-01-29 19:22:18 +0530

    Update 03_digital_clock.c

--- 03_digital_clock.c
+++ 03_digital_clock.c
@@ -3,10 +3,30 @@
 
 int main () {
 
+    time_t raw_time;
+    struct tm *current_time;
+    // Here we used a pointer because the localtime function returns a pointer to a tm structure
+    char buffer[100];
     
+    time(&raw_time);
+    current_time = localtime(&raw_time);
+    //localtime is a function from library time.h
+    // Convert the raw time to local time structure
 
-
-
+    // Format time into a human-readable string
+    // %A - Full weekday name
+    // %B - Full month name
+    // %d - Day of the month (01-31)
+    // %Y - Year
+    // %H - Hour (01-24)
+    // %I - Hour (01-12)
+    // %M - Minutes (00-59)
+    // %S - seconds (00-59)
+    // %p - AM/PM notation
+    strftime(buffer, sizeof(buffer), "Current Time : %I:%M:%S %p", current_time);
+    
+    // Print the formatted time
+    printf("%s\n", buffer);
 
 
     return 0;
